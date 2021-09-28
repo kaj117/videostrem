a2a186313bea1f3e0084cb71f51dcf49bfa1088e
kaj117 <70688258+kaj117@users.noreply.github.com>
2021-09-28 17:43:17 +0530

    Update entertainment.py

--- bot/entertainment.py
+++ bot/entertainment.py
@@ -61,19 +61,3 @@ async def dare(_, message):
     except Exception as ex:
         print(ex)
         await message.reply_text("something went wrong...")
-
-
-@Client.on_message(command(["lyric", f"lyric@{Veez.BOT_USERNAME}"]))
-async def lirik(_, message):
-    rep = await message.reply_text("🔎 **searching lyrics...**")
-    try:
-        if len(message.command) < 2:
-            await message.reply_text("**give a lyric name too !**")
-            return
-        query = message.text.split(None, 1)[1]
-        resp = requests.get(f"https://api-tede.herokuapp.com/api/lirik?l={query}").json()
-        result = f"{resp['data']}"
-        await rep.edit(result)
-    except Exception as ex:
-        print(ex)
-        await rep.edit("**Lyrics not found.** please give a valid song name !")
